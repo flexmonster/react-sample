a1910baa761fe8ba62df5874d1e91227e58a5b6f
nadia <se7enteenblue17@gmail.com>
2026-04-15 14:37:39 +0300

    minor fix

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import { useRef } from "react";
 import { FMFlexmonster } from "@flexmonster/react/next";
-import { FMFlexmonsterRef } from "@flexmonster/react";
 import { StateInputParams } from "@flexmonster/flexmonster";
-import "@flexmonster/flexmonster/flexmonster.css";
 
 export default function Home() {
-  const pivotRef = useRef<FMFlexmonsterRef>(null);
 
   const state: StateInputParams = {
     id: "fm-state",
@@ -44,7 +40,6 @@ export default function Home() {
     <div className="pivot-wrapper">
       <div className="pivot-container">
         <FMFlexmonster
-          ref={pivotRef}
           state={state}
           options={{width: "100%", height: "100%"}}
         />
